e06dea47256532b9e3b17f3ec754e07eae344f1e
nadireirem <nadireulsular@gmail.com>
2022-06-09 13:57:47 -0400

    new task

---
+++ src/test/java/com/cydeo/tests/officeHours/day04/Task_IQ.java
@@ -0,0 +1,19 @@
+package com.cydeo.tests.officeHours.day04;
+
+import org.apache.commons.lang3.Validate;
+import org.openqa.selenium.support.ui.Select;
+
+public class Task_IQ {
+
+    //            1. Navigate to: https://moneygaming.qa.gameaccount.com/
+    //            2. Click the JOIN NOW button to open the registration page
+    //        3. Select a title value from the dropdown
+    //        4. Enter your first name and surname in the form
+    //        5. Check the tickbox with text "I accept the Terms and Conditions and certify that I am over the age of 18.
+    //            6. Submit the form by clicking the JOIN NOW button
+    //        7. Validate that a validation message with text "This field is required" appears under the date of birth box
+
+
+
+
+}
